73fc6714ca6bebc256bf1c6e62feeb52090ba5bb
iza <izabella.lopes@monks.com>
2025-02-21 15:20:36 -0300

    ex 1+2

--- exercises/ex-1.go
+++ exercises/ex-1.go
@@ -1,1 +1,21 @@
 // 1- Escreva um programa em Go que imprima um triângulo de asteriscos de acordo com o número de linhas que o usuário desejar;
+package main
+
+import "fmt"
+
+func main(){
+    var l int
+   
+    fmt.Print("digite a quantidade de linhas:  ")
+    fmt.Scan(&l)
+
+    for i := 1; i <=l; i++ {
+       
+        for j := 0; j < i; j++ {
+            fmt.Print("*")
+
+        }
+        fmt.Println()
+    }
+   
+}
--- exercises/ex-2.go
+++ exercises/ex-2.go
@@ -1,1 +1,20 @@
 // 2- Modifique o programa anterior para imprimir um triângulo de asteriscos invertido;
+package main
+
+import "fmt"
+
+func main(){
+    var l int
+   
+    fmt.Print("digite a quantidade de linhas:  ")
+    fmt.Scan(&l)
+
+    for i := l; i > 0; i-- {
+        for j :=0 ; j < i; j++ {
+            fmt.Print("*")
+
+        }
+        fmt.Println()
+    }
+   
+}
