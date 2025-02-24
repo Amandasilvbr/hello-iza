5ca35f7f87b4390b8422a5a4bd96b13edbe99688
iza <izabella.lopes@monks.com>
2025-02-24 16:06:10 -0300

    att ex-10

--- exercises/ex-10.go
+++ exercises/ex-10.go
@@ -1,1 +1,27 @@
 // 10- Escreva uma função que recebe um número como entrada e verifica se ele é positivo, negativo ou zero. A função deve retornar uma mensagem indicando o resultado.
+
+
+package main
+
+import "fmt"
+
+func numero(a int) string {
+	switch {
+	case a > 0:
+		return "Positivo."
+	case a < 0:
+		return "Negativo."
+	default:
+		return "Zero."
+	}
+}
+
+func main() {
+	var a int
+
+	fmt.Print("Digite um número: ")
+	fmt.Scan(&a)
+
+	resultado := numero(a)
+	fmt.Println(resultado)
+}
