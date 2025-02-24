52575b27e13bdd233fab4d1e56e91eb3e95a2964
iza <izabella.lopes@monks.com>
2025-02-24 11:30:49 -0300

    att ex-6+ex-7

--- exercises/ex-6.go
+++ exercises/ex-6.go
@@ -1,1 +1,22 @@
 // 6- Escreva uma função que recebe uma string como entrada e conta o número de vogais presentes nessa string. Considere vogais apenas as letras "a", "e", "i", "o" e "u".
+
+package main
+
+import "fmt"
+
+func main() {
+    var b string
+    vogais := 0
+
+    fmt.Print("Escreva uma palavra: ")
+    fmt.Scan(&b)
+
+    for _, char := range b {
+        switch char {
+        case 'a', 'e', 'i', 'o', 'u':
+            vogais++
+        }
+    }
+
+    fmt.Printf("Quantidade de vogais:  %d\n", vogais)
+}
--- exercises/ex-7.go
+++ exercises/ex-7.go
@@ -1,1 +1,20 @@
 // 7- Escreva uma função que recebe uma string como entrada e retorna uma nova string que é a versão invertida da original.
+
+package main
+
+import "fmt"
+
+func main() {
+    var a string
+
+    fmt.Print("Digite uma palavra: ")
+    fmt.Scan(&a)
+
+    invertida := "" 
+
+    for i := len(a) - 1; i >= 0; i-- {
+        invertida += string(a[i]) 
+    }
+	
+    fmt.Println("Palavra invertida:", invertida)
+}
