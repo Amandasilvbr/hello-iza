09d0805daa1c454df88f345a8eb2168552d9c7ad
iza <izabella.lopes@monks.com>
2025-02-24 10:47:20 -0300

    att ex-4

--- exercises/ex-4.go
+++ exercises/ex-4.go
@@ -1,1 +1,18 @@
 // 4- Escreva um programa em Go que utilize uma estrutura de dados map para armazenar nomes de pessoas como chaves e suas idades como valores. Imprima o mapa completo.
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+	ni := map[string]int {
+		"Bob": 12,
+		"Maria": 29,
+		"Luis": 40,
+	}
+	for nome, idade := range ni {
+		fmt.Printf("%s: %d\n", nome, idade)
+	}
+
+}
